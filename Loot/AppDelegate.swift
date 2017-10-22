7415f2e309dfb49a1e2a562229cc8609c0c47c34
Ronald Mannak <ronaldmannak@me.com>
2017-10-21 22:20:06 -0700

    Initial commit

---
+++ Loot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Loot
+//
+//  Created by Ronald Mannak on 10/21/17.
+//  Copyright © 2017 Indisputable Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Loot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Loot
+//
+//  Created by Ronald Mannak on 10/21/17.
+//  Copyright © 2017 Indisputable Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
